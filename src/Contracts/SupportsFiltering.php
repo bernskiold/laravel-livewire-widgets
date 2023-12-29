0fc1fb0395925dea671a51ef226314510fcf5138
Erik Bernskiold <erik@bernskioldmedia.com>
2023-12-29 13:05:47 +0100

    wip

---
+++ src/Contracts/SupportsFiltering.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BernskioldMedia\LaravelLivewireWidgets\Contracts;
+
+interface SupportsFiltering
+{
+
+    public function getFilters(): array;
+
+    public function resetFilters(): void;
+
+    public function setFilter(string $key, $value): void;
+
+    public function removeFilter(string $key): void;
+
+    public function hasFilter(string $key): bool;
+
+    public function getFilter(string $key): mixed;
+
+}
